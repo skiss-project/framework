35767486aa5696e3df86070f8ba19da6f819131e
Simon Wendel <mail@simonwendel.se>
2018-08-16 10:06:21 +0000

    chore: switch to full FX 4.6.1
    .NET Standard is dropped right now and we will focus on full framework.

---
+++ src/Skiss.Lib/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Skiss.Lib")]
+[assembly: AssemblyProduct("Skiss.Lib")]
+[assembly: AssemblyCopyright("Copyright © Simon Wendel 2018")]
+[assembly: ComVisible(false)]
+[assembly: Guid("48c5ecaa-bfa5-4aeb-b988-a7242e6b2e20")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
