745d26d4780279639a9d34b33181f62186f41910
Simon Wendel <mail@simonwendel.se>
2018-08-26 15:40:43 +0200

    style: re-name tests for getters to remove suppression attrs

--- src/Skiss.Tests/DriverManagerTests.cs
+++ src/Skiss.Tests/DriverManagerTests.cs
@@ -1,20 +1,15 @@
 ﻿namespace Skiss.Tests
 {
     using System;
-    using System.Diagnostics.CodeAnalysis;
     using FluentAssertions;
     using NUnit.Framework;
     using Skiss.Driver.UIAutomation;
     using Skiss.Lib;
 
-    [SuppressMessage(
-        "StyleCop.CSharp.NamingRules", 
-        "SA1300:ElementMustBeginWithUpperCaseLetter", 
-        Justification = "Testing a getter here, the actual name is get_Current")]
     public class DriverManagerTests
     {
         [Test]
-        public void get_Current_GivenDriverNotSet_ThrowsException()
+        public void CurrentGetter_GivenDriverNotSet_ThrowsException()
         {
             DriverManager.Current = null;
             Action gettingTheUnsetDriver = () => { var d = DriverManager.Current; };
@@ -22,7 +17,7 @@
         }
 
         [Test]
-        public void get_Current_GivenDriverSet_DoesNotThrowException()
+        public void CurrentGetter_GivenDriverSet_DoesNotThrowException()
         {
             var driver = new UIAutomationDriver();
             DriverManager.Current = driver;
