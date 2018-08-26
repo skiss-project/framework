698f3b959bd317571c0999a5ee13944303ef0e21
Simon Wendel <mail@simonwendel.se>
2018-08-26 14:20:25 +0200

    feat: add a target WinForms app to run tests against

---
+++ src/Skiss.AutomationTarget.WinForms/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Skiss.AutomationTarget.WinForms
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+        private void invoke_Click(object sender, EventArgs e)
+        {
+            results.Text = (int.Parse(firstOperand.Text) + int.Parse(secondOperand.Text)).ToString();
+        }
+    }
+}
