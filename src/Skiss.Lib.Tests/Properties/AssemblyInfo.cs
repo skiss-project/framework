e358c2c18c4678f41ce80bf9725e279c7059efcd
Simon Wendel <mail@simonwendel.se>
2018-08-26 15:44:38 +0200

    chore: rename Skiss.Tests -> Skiss.Lib.Tests, to match intent

--- src/Skiss.Lib.Tests/Properties/AssemblyInfo.cs
+++ src/Skiss.Lib.Tests/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Skiss.Tests")]
-[assembly: AssemblyProduct("Skiss.Tests")]
+[assembly: AssemblyTitle("Skiss.Lib.Tests")]
+[assembly: AssemblyProduct("Skiss.Lib.Tests")]
 [assembly: AssemblyCopyright("Copyright © Simon Wendel 2018")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6dba4bfa-2c11-47ba-8dd4-d9d163093b95")]
