82e33f72a27682e2da1fc66d33c03a83b4e84c9e
Simon Wendel <mail@simonwendel.se>
2018-08-31 18:35:47 +0200

    chore: target .NET Standard 1.0 in Skiss.Framework

---
+++ src/Skiss.Framework/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCopyright("Copyright © Simon Wendel 2018")]
+[assembly: InternalsVisibleTo("Skiss.Framework.Tests")]
