374f5a7a33d4dd8832f46bcc45d5430d5f1ba6f6
Simon Wendel <mail@simonwendel.se>
2018-09-05 14:11:11 +0200

    test: cover most of AutomationElementProxy at least for sad paths

---
+++ src/Skiss.Driver.UIAutomation.Tests/AutomationElementProxyTests.cs
@@ -0,0 +1,57 @@
+﻿namespace Skiss.Driver.UIAutomation.Tests
+{
+    using System;
+    using System.Windows.Automation;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    public class AutomationElementProxyTests
+    {
+        private AutomationElementProxy sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new AutomationElementProxy();
+        }
+
+        [Test]
+        public void Constructor_WhenNullaryInvocation_ConstructsRootProxy()
+        {
+            var rootInfo = AutomationElement.RootElement.Current;
+            var proxiedInfo = sut.Current;
+
+            proxiedInfo.Should().Be(rootInfo);
+        }
+
+        [Test]
+        public void FindAll_GivenNullCondition_ThrowsException()
+        {
+            Action finding = () => sut.FindAll(TreeScope.Subtree, null);
+
+            finding
+                .Should().ThrowExactly<ArgumentNullException>()
+                .Which.ParamName.Should().Be("condition");
+        }
+
+        [Test]
+        public void FindFirst_GivenNullCondition_ThrowsException()
+        {
+            Action finding = () => sut.FindFirst(TreeScope.Subtree, null);
+
+            finding
+                .Should().ThrowExactly<ArgumentNullException>()
+                .Which.ParamName.Should().Be("condition");
+        }
+
+        [Test]
+        public void TryGetCurrentPattern_GivenNullPattern_ThrowsException()
+        {
+            Action trying = () => sut.TryGetCurrentPattern(null, out var patternObj);
+
+            trying
+                .Should().ThrowExactly<ArgumentNullException>()
+                .Which.ParamName.Should().Be("pattern");
+        }
+    }
+}
