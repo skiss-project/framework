4415181995497aa5bee3dc725a5cfb887644500d
Simon Wendel <mail@simonwendel.se>
2018-09-05 13:45:02 +0200

    feat: add a simple Guard class to driver project

---
+++ src/Skiss.Driver.UIAutomation/Guard.cs
@@ -0,0 +1,15 @@
+﻿namespace Skiss.Driver.UIAutomation
+{
+    using System;
+
+    internal static class Guard
+    {
+        public static void AgainstNull(object obj, string name)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(name);
+            }
+        }
+    }
+}
