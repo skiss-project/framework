69b64eed8dfddee195ce9b5d324caf2f22e53ccd
Simon Wendel <mail@simonwendel.se>
2018-09-07 19:27:04 +0200

    test: make explicit that element can be null in MissingCapabilityException

--- src/Skiss.Framework.Tests/MissingCapabilityExceptionTests.cs
+++ src/Skiss.Framework.Tests/MissingCapabilityExceptionTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Skiss.Framework.Tests
 {
+    using System;
     using FluentAssertions;
     using NUnit.Framework;
     using Skiss.Framework;
@@ -16,6 +17,13 @@
 
             sut.Capability.Should().Be(expectedCapability);
             sut.Element.Should().BeSameAs(expectedElement);
+        }
+
+        [Test]
+        public void Constructor_GivenNullElement_DoesNotThrowException()
+        {
+            var sut = new MissingCapabilityException(Capability.Readable, null);
+            sut.Element.Should().BeNull();
         }
     }
 }
