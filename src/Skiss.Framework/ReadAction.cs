a5d7fe3e6391312d53b928555e3f2fd7aec2d8da
Simon Wendel <mail@simonwendel.se>
2018-11-08 19:51:24 +0100

    fact: remove empty nullary constructor

--- src/Skiss.Framework/ReadAction.cs
+++ src/Skiss.Framework/ReadAction.cs
@@ -19,10 +19,6 @@ namespace Skiss.Framework
     public abstract class ReadAction<T> : ElementAction<T>
         where T : Element<T>
     {
-        public ReadAction()
-        {
-        }
-
         public string Text
         {
             get
